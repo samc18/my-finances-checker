c2323d97dc3a9e5091174b52ef61231b5018b9bb
Sam Candela <byfyos72@gmail.com>
2022-10-30 21:36:55 -0500

    style: add icons in header for small screens plus fix order category components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,11 @@ function Header() {
                 <ul>
                     <Link to='fundamentals'>
                         <li>Fundamentals</li>
+                        <i class="fa-solid fa-book"></i>
                     </Link>
                     <Link to='calculator'>
                         <li>Calculator</li>
+                        <i class="fa-solid fa-calculator"></i>
                     </Link>                    
                 </ul>
             </nav>
