002e475bf91bb258de4b79f765d5ec502990212f
Alfiya Siddique <alfiyasiddique@1708>
2022-12-28 19:19:10 +0530

    Update Fundamentals page to new version and solve text overflow issue

--- src/pages/Fundamentals.jsx
+++ src/pages/Fundamentals.jsx
@@ -2,96 +2,65 @@ import { Text } from '../components/LanguageProvider'
 import LanguageSelector from '../components/LanguageSelector'
 
 function Fundamentals() {
-  return (
-    <main className="fundamentals">
-      <div className="fundamentals__title">
-        <h1>The 50/30/20 rule explained!</h1>
-        <p className="fundamentals__description">
-          A popular budgeting method that distributes income into 3 main
-          categories.
-        </p>
-      </div>
+    return (
+        <main className='fundamentals'>
+            <LanguageSelector />
 
-      <div className="fundamentals__container main">
-        <h2 className="fundamentals__subtitle">Monthly income.</h2>
-        <i className="fa-solid fa-sack-dollar"></i>
-        <p className="fundamentals__description">
-          This amount is your monthly income after taxes. There can be more than
-          one. If it's the salary from work just remove the taxes not other
-          amounts that are usually deducted.
-        </p>
-      </div>
+            <h1 className='fundamentals__title'><Text tid='fundamentalsTitle' /></h1>
+            <p className='fundamentals__description'><Text tid='fundamentalsTitleDescription' /></p>
 
-      <div className="grid">
-        <div className="fundamentals__container">
-          <h2 className="fundamentals__subtitle">50% of your income: needs.</h2>
-          <i className="fa-solid fa-house"></i>
-          <p className="fundamentals__description">
-            Necessities are the expenses we can not live without. Here are some
-            examples:
-          </p>
-          <ul className="fundamentals__list">
-            <li className="fundamentals__item">Housing.</li>
-            <li className="fundamentals__item">Food.</li>
-            <li className="fundamentals__item">Transportation.</li>
-            <li className="fundamentals__item">Insurance.</li>
-            <li className="fundamentals__item">
-              Minimum loan payments. Anything beyond the minimum goes into
-              savings.
-            </li>
-            <li className="fundamentals__item">
-              Expenses you need to cover so you can work, child care for
-              example.
-            </li>
-          </ul>
-        </div>
+            <div className='fundamentals__container main'>
+                <h2 className='fundamentals__subtitle'><Text tid='fundamentalsIncomeTitle' /></h2>
+                <i className="fa-solid fa-sack-dollar"></i>
+                <p className='fundamentals__description'><Text tid='fundamentalsIncomeDescription' /></p>
+            </div>
+            
+            <div className="grid">
+            <div className='fundamentals__container'>
+                <h2 className='fundamentals__subtitle'><Text tid='fundamentalsNeedsTitle' /></h2>
+                <i className="fa-solid fa-house"></i>
+                <p className='fundamentals__description'><Text tid='fundamentalsNeedsDescription' /></p>
+            <ul className='fundamentals__list'>
+                <li className='fundamentals__item'><Text tid='fundamentalsNeedsExample1' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsNeedsExample2' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsNeedsExample3' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsNeedsExample4' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsNeedsExample5' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsNeedsExample6' /></li>
+            </ul>
+            </div>
 
-        <div className="fundamentals__container">
-          <h2 className="fundamentals__subtitle">30% of your income: wants.</h2>
-          <i className="fa-solid fa-plane-departure"></i>
-          <p className="fundamentals__description">
-            Wants are the extras we do for fun. They are not essential as in we
-            can live without them. Here are some examples:
-          </p>
-          <ul className="fundamentals__list">
-            <li className="fundamentals__item">Monthly subscriptions.</li>
-            <li className="fundamentals__item">Travel.</li>
-            <li className="fundamentals__item">Entertainment.</li>
-            <li className="fundamentals__item">Meals out.</li>
-          </ul>
-        </div>
-        <div className="fundamentals__container">
-          <h2 className="fundamentals__subtitle">
-            20% of your income: savings.
-          </h2>
-          <i className="fa-solid fa-piggy-bank"></i>
-          <p className="fundamentals__description">
-            Savings is the category we set aside to prepare for the future. We
-            start by repaying debt. High-interest accounts should be targeted
-            first. Here are some examples:
-          </p>
-          <ul className="fundamentals__list">
-            <li className="fundamentals__item">
-              Starting and growing an emergency fund.
-            </li>
-            <li className="fundamentals__item">Saving for retirement.</li>
-            <li className="fundamentals__item">Paying off debt.</li>
-          </ul>
-        </div>
-      </div>
-
-      <p className="fundamentals__note">
-        This page is based on the{" "}
-        <a
-          href="https://www.nerdwallet.com/article/finance/nerdwallet-budget-calculator"
-          target="_blank"
-        >
-          <strong>article</strong>
-        </a>{" "}
-        from NerdWallet.
-      </p>
-    </main>
-  );
+            <div className='fundamentals__container'>
+                <h2 className='fundamentals__subtitle'><Text tid='fundamentalsWantsTitle' /></h2>
+                <i className="fa-solid fa-plane-departure"></i>
+            
+            <p className='fundamentals__description'><Text tid='fundamentalsWantsDescription' /></p>
+            <ul className='fundamentals__list'>
+                <li className='fundamentals__item'><Text tid='fundamentalsWantsExample1' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsWantsExample2' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsWantsExample3' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsWantsExample4' /></li>
+            </ul>
+            </div>
+            <div className='fundamentals__container'>
+                <h2 className='fundamentals__subtitle'><Text tid='fundamentalsSavingsTitle' /></h2>
+                <i className="fa-solid fa-piggy-bank"></i>
+            <p className='fundamentals__description'><Text tid='fundamentalsSavingsDescription' /></p>
+            <ul className='fundamentals__list'>
+                <li className='fundamentals__item'><Text tid='fundamentalsSavingsExample1' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsSavingsExample2' /></li>
+                <li className='fundamentals__item'><Text tid='fundamentalsSavingsExample3' /></li>
+            </ul>
+            </div>
+            </div>
+            <p className='fundamentals__note'><Text tid='fundamentalsCredits1' />&nbsp;
+                <a href='https://www.nerdwallet.com/article/finance/nerdwallet-budget-calculator' target='_blank'>
+                    <strong><Text tid='fundamentalsCredits2' /></strong>&nbsp;
+                </a>
+                <Text tid='fundamentalsCredits3' />
+            </p>
+        </main>
+    )
 }
 
-export default Fundamentals;
+export default Fundamentals
