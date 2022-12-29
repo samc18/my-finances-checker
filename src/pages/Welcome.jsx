0f9b1175493b677c5a2918e74659d1528ff05b4c
Sam Candela <93607710+samc18@users.noreply.github.com>
2022-12-28 20:46:10 -0500

    fix: add welcome message space

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,9 +3,7 @@ import { Text } from '../components/LanguageProvider'
 function Welcome(props) {
     return (
         <main className='welcome'>
-            <p className='welcome__title'><Text tid="welcomeMessage1"/> 
-                <strong><Text tid="welcomeMessage2" /></strong>
-            </p>
+            <p className='welcome__title'><Text tid="welcomeMessage1"/> <strong><Text tid="welcomeMessage2" /></strong></p>
 
             <button className='welcome__btn' onClick={props.onClick}>
                 <span className="btnText"> <Text tid="welcomeButton" /> </span>
